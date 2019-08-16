6fe004ba18e35e54df7ccc246895c7d093835b99
Jan Nyholm <jan.nyholm@codento.com>
2019-08-16 14:35:19 +0300

    ssr poc

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const express = require('express');
-const path = require('path');
-
+import express from 'express'
+import App from './src/App'
+import path from 'path';
+import fs from 'fs';
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+const router = express.Router()
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+const serverRenderer = (req, res, next) => {
+  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('An error occurred')
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    )
+  })
+}
+router.use('^/$', serverRenderer)
+
+// Serve static files from the React app
+router.use(
+  express.static(path.resolve(__dirname, 'build'), { maxAge: '30d' })
+)
+
+// tell the app to use the above rules
+app.use(router)
 
 const port = process.env.PORT || 5000;
 app.listen(port);
--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <React.Suspense fallback={Loader}>
           <Parent/>
-        </React.Suspense>
       </header>
     </div>
   );
