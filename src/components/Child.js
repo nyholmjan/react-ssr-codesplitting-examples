1dbf7a10f5c3f18cdb522d5391aa9012a017e561
Jan Nyholm <jan.nyholm@codento.com>
2019-08-16 13:08:01 +0300

    max width for sora

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import "./Child.css"
+
 const Child = () => {
   return(
     <>
     <p>Child</p>
-    <img src="sortsa.jpg" alt="Sortsa"/>
+    <img src="sortsa.jpg" alt="Sortsa" className={"soran-parta"}/>
     </>
   )
 }
