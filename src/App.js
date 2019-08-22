60f4cafe7bfbb4ace943bbbe9dcf00f09cf765f0
Jan Nyholm <jan.nyholm@codento.com>
2019-08-22 14:47:18 +0300

    apps together

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import Parent from './components/Parent'
-import Loader from "./components/Loader"
+import { Route, Link, Switch } from 'react-router-dom'
+import Sortsa from "./components/Sortsa"
+import Story from "./components/Story"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Parent/>
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Tabs component={'div'} value={location.pathname}>
+              <Tab value={"/"} label={'Main'} component={Link} to={'/'}/>
+              <Tab value={"/story"} label={'Story'} component={Link} to={'/story'} />
+              <Tab value={"/sortsa"} label={'Sortsa'} component={Link} to={'/sortsa'} />
+            </Tabs>
+          )} />
+          <Switch>
+            <Route exact path={"/"} render={() => <p>Frontpage</p>} />
+            <Route path={"/story"} component={Story} />
+            <Route path={"/sortsa"} component={Sortsa} />
+          </Switch>
       </header>
     </div>
   );
